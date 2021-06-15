8cc85add79a4785535cb616e573894c712382137
Gustavo Camello <gucamello@gmail.com>
2021-06-15 11:15:22 +0100

    Using AJAX to make the requests

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,15 +3,21 @@ class StocksController < ApplicationController
   def search
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
-      if @stock
-      render 'users/my_portfolio'
+        if @stock
+          respond_to do |format|
+            format.js { render partial: 'users/result' }
+        end
       else
-        flash[:alert] = "Please enter a valid symbol"
-        redirect_to my_portfolio_path
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a valid symbol"
+          format.js { render partial: 'users/result' }
+        end
       end
     else
-      flash[:alert] = "Please enter a symbol to search"
-      redirect_to my_portfolio_path
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a symbol to search"
+        format.js { render partial: 'users/result' }
+      end
     end
   end
 
