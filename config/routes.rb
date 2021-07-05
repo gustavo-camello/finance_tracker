8994e0e2a91e803c6324957fe2aa676f04d84bd3
Gustavo Camello <gucamello@gmail.com>
2021-07-05 11:03:13 +0100

    Create friends profile page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :user_stocks, only: [:create, :destroy]
   resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
 
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
